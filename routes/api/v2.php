c6365e1a9e64cd347221a763803dce3bacc795a4
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-02-06 13:22:37 -0600

    Adding Summaries endpoint

--- routes/api/v2.php
+++ routes/api/v2.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Api\V2\CompleteTaskController;
 use App\Http\Controllers\Api\V2\TaskController;
+use App\Http\Controllers\Api\V2\SummaryController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v2')->group(function(){
     // GET, PUT, DELETE
     Route::apiResource('/tasks', TaskController::class);
+    Route::get('/summaries', SummaryController::class);
     // PATCH: Partially update a resource, which means that a portion of the resource 
     //will be changed, instead of the entire resource.
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
